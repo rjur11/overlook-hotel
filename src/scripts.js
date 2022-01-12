386ed1aa14c44da5c17647161c1e1d10e695e329
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-01-12 13:35:15 -0500

    Add logo

--- src/scripts.js
+++ src/scripts.js
@@ -2,10 +2,11 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+import "./css/base.scss";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import "./images/turing-logo.png";
+import "./images/hotel-room.jpg";
+import "./images/transparent-logo.png";
 
-
-console.log('This is the JavaScript entry file - your code begins here.');
+console.log("This is the JavaScript entry file - your code begins here.");
