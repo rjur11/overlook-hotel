f80b835fd6601cbf871ba180723c9e7a1d1ede7e
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-01-13 13:15:37 -0500

    Add more room tests

--- test/room-test.js
+++ test/room-test.js
@@ -3,6 +3,7 @@ const Room = require("../src/classes/Room.js");
 
 describe("Room", () => {
   let room1;
+  let room2;
 
   beforeEach(() => {
     room1 = new Room({
@@ -13,6 +14,14 @@ describe("Room", () => {
       numBeds: 1,
       costPerNight: 358.4,
     });
+    room2 = new Room({
+      number: 2,
+      roomType: "suite",
+      bidet: false,
+      bedSize: "full",
+      numBeds: 2,
+      costPerNight: 477.38,
+    });
   });
 
   it("Should be a function", () => {
@@ -20,5 +29,19 @@ describe("Room", () => {
   });
   it("should be an instance of Room", () => {
     expect(room1).to.be.an.instanceOf(Room);
+  });
+  it("should have a number", () => {
+    expect(room1.number).to.equal(1);
+  });
+  it("should have a room type", () => {
+    expect(room1.roomType).to.equal("residential suite");
+    expect(room2.roomType).to.equal("suite");
+  });
+  it("should tell the user if a bidet is present", () => {
+    expect(room1.bidet).to.equal(true);
+    expect(room2.bidet).to.equal(false);
+  });
+  it("should tell the user the size of bed", () => {
+    expect(room1.bedSize).to.equal("queen");
   });
 });
