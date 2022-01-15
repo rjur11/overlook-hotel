a3f46a593c3dde1162c90c164a13aa11cd20960f
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-01-14 19:15:22 -0500

    Begin render logic between domUpdates and scripts

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,3 +1,24 @@
-const domUpdates = {};
+const pastBookingsUl = document.querySelector(".past-bookings-details");
+const currentBookingsUl = document.querySelector(".current-bookings-details");
+const futureBookingsUl = document.querySelector(".future-bookings-details");
+
+const createBookingCard = (booking) => {
+  const li = document.createElement("li");
+  li.innerText = `${booking.date} : ${booking.roomNumber}`;
+  return li;
+};
+const addBookingsCardsToUl = (ul, bookings) => {
+  bookings.map(createBookingCard).forEach((card) => {
+    ul.appendChild(card);
+  });
+};
+
+const domUpdates = {
+  renderBookings(pastBookings, currentBookings, futureBookings) {
+    addBookingsCardsToUl(pastBookingsUl, pastBookings);
+    addBookingsCardsToUl(currentBookingsUl, currentBookings);
+    addBookingsCardsToUl(futureBookingsUl, futureBookings);
+  },
+};
 
 export default domUpdates;
